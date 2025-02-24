7f83cfeff7740e65c4e15d35383545cbeda73b16
JangBJ <qudwnd1001@naver.com>
2025-02-24 20:20:10 +0900

    초기 세팅

---
+++ backend/JiShop/src/test/java/com/jishop/JiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- backend/log/src/main/java/com/example/log/config/webConfig.java
+++ backend/log/src/main/java/com/example/log/config/webConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class webConfig implements WebMvcConfigurer {
 
 }
