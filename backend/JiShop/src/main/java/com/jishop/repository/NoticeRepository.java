4f692289ca173bdbce97d50ce99d83f2437b02ff
kiteof_park <kiteof_park@naver.com>
2025-02-28 11:07:11 +0900

    🎨 style: 어노테이션 컨벤션 준수
    🔗 Resolves: #be/feat/16

--- backend/JiShop/src/main/java/com/jishop/repository/NoticeRepository.java
+++ backend/JiShop/src/main/java/com/jishop/repository/NoticeRepository.java
@@ -4,7 +4,9 @@ import com.jishop.common.exception.DomainException;
 import com.jishop.common.exception.ErrorType;
 import com.jishop.domain.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
     default Notice findOne(Long id){
