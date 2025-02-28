b4c0eb14783a82c26b10eb6c27c0d0ed17209487
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-02-28 12:20:19 +0900

    🛠 refactor: 중복코드 제거 및 스케쥴러 인터페이스 분리
    - 스케쥴러의 인터페이스분리
    - build.gradle 파일 validation 중복 제거
    
    🔗 Resolves: #12

--- backend/JiShop/src/main/java/com/jishop/scheduler/ProductDataScheduler.java
+++ backend/JiShop/src/main/java/com/jishop/scheduler/ProductDataScheduler.java
@@ -1,24 +1,6 @@
 package com.jishop.scheduler;
 
-import com.jishop.service.impl.ProductDataServiceImpl;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+public interface ProductDataScheduler {
 
-import java.time.LocalDateTime;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class ProductDataScheduler {
-
-    private final ProductDataServiceImpl productDataServiceImpl;
-
-//    @Scheduled(cron = "0 0 1 * * ?")
-    @Scheduled(cron = "*/10 * * * * *")
-    public void fetchProductData() {
-        System.out.println("Product data fetched at: " + LocalDateTime.now());
-        productDataServiceImpl.fetchAndSaveProductData();
-    }
+    void fetchProductData();
 }
