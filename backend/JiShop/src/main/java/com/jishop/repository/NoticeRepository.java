713454cb1fc61556248514abfd73cd33a28be465
kiteof_park <kiteof_park@naver.com>
2025-03-04 14:15:51 +0900

    ✨ feat: 검색 키워드 기반 공지사항 검색 레포지토리 구현
    - 검색 키워드가 본문에 포함된 경우,
    - 검색 키워드가 제목에 포함된 경우 Notice 데이터 반환
    
    🔗 Resolves: #be/feat/27

--- backend/JiShop/src/main/java/com/jishop/repository/NoticeRepository.java
+++ backend/JiShop/src/main/java/com/jishop/repository/NoticeRepository.java
@@ -3,10 +3,13 @@ package com.jishop.repository;
 import com.jishop.common.exception.DomainException;
 import com.jishop.common.exception.ErrorType;
 import com.jishop.domain.Notice;
+import com.jishop.dto.NoticeResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -30,4 +33,11 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
             "END DESC, " +
             "n.createdAt DESC")
     Page<Notice> findAll(Pageable pageable);
+
+    // 검색 키워드 기반 공지사항 조회
+        // 공지사항 게시글에 키워드가 포함된 경우
+        // 공지사항 제목에 키워드가 포함된 경우
+    @Query("SELECT n FROM Notice n WHERE n.title LIKE %:keyword% OR n.content LIKE %:keyword%")
+    Page<Notice> searchByKeyword(@Param("keyword")String keyword, Pageable pageable);
+
 }
