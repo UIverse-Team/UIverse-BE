e1674f15afa4ea0257da50c1679042f8d8fe4204
kiteof_park <kiteof_park@naver.com>
2025-03-09 20:50:47 +0900

    🐛 fix: FAQ 파일 구조 변경

--- backend/JiShop/src/main/java/com/jishop/repository/FaqRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jishop.repository;
-
-import com.jishop.domain.Faq;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FaqRepository extends JpaRepository<Faq, Long> {
-}
