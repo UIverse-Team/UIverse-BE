21b9da1d7c9ddc829d00932d2e935a1a5c221308
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-03-11 19:26:42 +0900

    ✨ feat: 상품, 스토어, 카테고리 데이터 엔티티 생성
    - 상품, 스토어, 카테고리 데이터 엔티티 생성 및 웹 크롤러 생성
    
    🔗 Resolves: #61

--- backend/JiShop/src/main/java/com/jishop/repository/ProductRepository.java
+++ backend/JiShop/src/main/java/com/jishop/repository/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.jishop.repository;
 
-import com.jishop.domain.ProductData;
+import com.jishop.domain.product.BatchProductData;
+import com.jishop.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<ProductData, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
