400f322aa5902af3ebc2b32765c8a10b1de9d028
JangBJ <qudwnd1001@naver.com>
2025-03-13 20:14:37 +0900

    🔧 chore: 코멘트 반영 🔗 Resolves: #72

--- backend/JiShop/src/main/java/com/jishop/member/dto/response/UserIdResponse.java
+++ backend/JiShop/src/main/java/com/jishop/member/dto/response/UserIdResponse.java
@@ -3,7 +3,7 @@ package com.jishop.member.dto.response;
 public record UserIdResponse(
         Long id
 ) {
-    public static UserIdResponse of(Long id) {
+    public static UserIdResponse from(Long id) {
         return new UserIdResponse(id);
     }
 }
