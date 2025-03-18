69f94d31dfd0c57e4af4e7739d364fa1c2cc7e80
Soyun_p <ehouse190@gmail.com>
2025-03-18 17:26:42 +0900

    ✨ feat: 회원 장바구니
    - 회원 장바구니 조회
    - 회원 장바구니 삭제 추가
    - 회원 장바구니 상품 수량 변경
    - 회원 장바구니 상품 삭제
    
    🔗 Resolves: #be/feat/114

---
+++ backend/JiShop/src/main/java/com/jishop/cart/controller/CartController.java
@@ -0,0 +1,18 @@
+package com.jishop.cart.controller;
+
+import com.jishop.cart.domain.Cart;
+import com.jishop.cart.dto.AddCartRequest;
+import com.jishop.cart.dto.CartDetailResponse;
+import com.jishop.cart.dto.CartResponse;
+import com.jishop.cart.dto.UpdateCartRequest;
+import com.jishop.member.domain.User;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "장바구니 API")
+public interface CartController {
+    ResponseEntity<CartResponse> getCartItems(User user);
+    ResponseEntity<CartDetailResponse> addCartItem(User user, AddCartRequest request);
+    ResponseEntity<CartDetailResponse> updateCartItem(User user, UpdateCartRequest request);
+    ResponseEntity<String> removeCartItem(User user, Long cartId);
+}
--- backend/JiShop/src/main/java/com/jishop/saleproduct/repository/SaleProductRepository.java
+++ backend/JiShop/src/main/java/com/jishop/saleproduct/repository/SaleProductRepository.java
@@ -17,4 +17,6 @@ public interface SaleProductRepository extends JpaRepository<SaleProduct, Long> 
             "LEFT JOIN FETCH sp.stock " +
             "WHERE sp.id IN :ids")
     List<SaleProduct> findAllByIdsForOrder(@Param("ids") List<Long> ids);
+
+    boolean existsByNameContaining(String keyword);
 }
