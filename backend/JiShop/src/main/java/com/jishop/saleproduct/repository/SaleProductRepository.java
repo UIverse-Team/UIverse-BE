4086327161eb61f74f326578456d8456be73c8e4
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-03-20 21:45:26 +0900

    merge

--- backend/JiShop/src/main/java/com/jishop/saleproduct/repository/SaleProductRepository.java
+++ backend/JiShop/src/main/java/com/jishop/saleproduct/repository/SaleProductRepository.java
@@ -20,6 +20,4 @@ public interface SaleProductRepository extends JpaRepository<SaleProduct, Long> 
             "LEFT JOIN FETCH sp.stock " +
             "WHERE sp.id IN :ids")
     List<SaleProduct> findAllByIdsForOrder(@Param("ids") List<Long> ids);
-
-    boolean existsByNameContaining(String keyword);
 }
