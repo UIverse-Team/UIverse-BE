527c8037b561cd4162c7130643ebfcdf1303c566
jihaneol <6531z@naver.com>
2025-03-22 20:11:03 +0900

    ✨ feat: 이미지 기능 개발
    🔗 Resolves: #131

--- backend/JiShop/src/main/java/com/jishop/image/S3ImagesService.java
+++ backend/JiShop/src/main/java/com/jishop/image/S3ImagesService.java
@@ -1,9 +1,11 @@
 package com.jishop.image;
 
+import com.jishop.review.domain.embed.ImageUrls;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface S3ImagesService {
     List<String> upload(List<MultipartFile> files);
+    void delete(ImageUrls imageUrls);
 }
--- backend/JiShop/src/main/java/com/jishop/image/S3ImagesServiceImpl.java
+++ backend/JiShop/src/main/java/com/jishop/image/S3ImagesServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jishop.image;
 
+import com.jishop.review.domain.embed.ImageUrls;
 import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.S3Client;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -21,6 +23,14 @@ public class S3ImagesServiceImpl implements S3ImagesService{
     public List<String> upload(List<MultipartFile> files) {
 
 
+
+
         return List.of();
+    }
+
+    @Override
+    public void delete(ImageUrls imageUrls) {
+
+
     }
 }
