74abfdec09ace2be4cad079d753965623edcd100
JangBJ <qudwnd1001@naver.com>
2025-03-22 17:29:12 +0900

    🛠️ refactor: 회원가입 수정 🔗 Resolves: #141

--- backend/JiShop/src/main/java/com/jishop/member/service/UserService.java
+++ backend/JiShop/src/main/java/com/jishop/member/service/UserService.java
@@ -3,13 +3,11 @@ package com.jishop.member.service;
 import com.jishop.member.domain.User;
 import com.jishop.member.dto.request.FindUserRequest;
 import com.jishop.member.dto.request.SignUpFormRequest;
-import com.jishop.member.dto.request.Step1Request;
 import com.jishop.member.dto.response.FindUserResponse;
 
 public interface UserService {
 
     User oauthLogin(OAuthProfile profile);
     void signUp(SignUpFormRequest form);
-    void emailcheck(Step1Request request);
     FindUserResponse findUser(FindUserRequest request);
 }
--- backend/JiShop/src/main/java/com/jishop/member/service/impl/UserServiceImpl.java
+++ backend/JiShop/src/main/java/com/jishop/member/service/impl/UserServiceImpl.java
@@ -4,12 +4,12 @@ import com.jishop.common.exception.DomainException;
 import com.jishop.common.exception.ErrorType;
 import com.jishop.member.domain.LoginType;
 import com.jishop.member.domain.User;
-import com.jishop.member.dto.request.*;
-import com.jishop.member.dto.response.*;
+import com.jishop.member.dto.request.FindUserRequest;
+import com.jishop.member.dto.request.SignUpFormRequest;
+import com.jishop.member.dto.response.FindUserResponse;
 import com.jishop.member.repository.UserRepository;
 import com.jishop.member.service.OAuthProfile;
 import com.jishop.member.service.UserService;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,6 +23,7 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User oauthLogin(OAuthProfile profile) {
@@ -30,6 +31,7 @@ public class UserServiceImpl implements UserService {
         LoginType provider = LoginType.valueOf(profile.getProvider().toUpperCase());
 
         Optional<User> existingUser = userRepository.findByLoginIdAndProvider(loginId, provider);
+
         if (existingUser.isPresent()) {
             return existingUser.get();
         } else {
@@ -50,30 +52,18 @@ public class UserServiceImpl implements UserService {
         }
     }
 
-    public void emailcheck(Step1Request request){
-        if(userRepository.findByLoginId(request.email()).isPresent()){
-            throw new DomainException(ErrorType.EMAIL_DUPLICATE);
-        }
-    }
+    public void signUp(SignUpFormRequest request) {
+        String password = passwordEncoder.encode(request.password());
+        SignUpFormRequest upFormRequest = request.withPassword(password);
 
-    public void signUp(SignUpFormRequest form) {
-        userRepository.save(form.toEntity());
+        userRepository.save(upFormRequest.toEntity());
     }
 
     public FindUserResponse findUser(FindUserRequest request){
         User user = userRepository.findByPhone(request.phone())
                 .orElseThrow(() -> new DomainException(ErrorType.USER_NOT_FOUND));
-
         if(user.isDeleteStatus()) throw new DomainException(ErrorType.USER_NOT_FOUND);
 
         return FindUserResponse.of(user);
-    }
-
-    // user id 들고오는 서비스 필요
-    public UserIdResponse findUserId(EmailRequest request){
-        User user = userRepository.findByLoginId(request)
-                .orElseThrow(() -> new DomainException(ErrorType.USER_NOT_FOUND));
-
-        return UserIdResponse.from(user.getId());
     }
 }
