32a9edec70f27f96413d7bc0d0090c063be7967c
Soyun_p <ehouse190@gmail.com>
2025-03-23 17:20:48 +0900

    🛠️ refactor: 비회원 회원 End-point 분리
    🔗 Resolves: #be/feat/151

--- backend/JiShop/src/main/java/com/jishop/order/dto/OrderRequest.java
+++ backend/JiShop/src/main/java/com/jishop/order/dto/OrderRequest.java
@@ -1,14 +1,14 @@
 package com.jishop.order.dto;
 
 import com.jishop.address.dto.AddressRequest;
-import com.jishop.member.domain.User;
-import com.jishop.order.domain.Order;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
-import java.util.*;
+import java.util.List;
 
 public record OrderRequest(
+        @NotNull(message = "주소 정보는 필수입니다.")
         @Valid AddressRequest address,
         @NotEmpty(message = "주문 상품 목록은 비어있을 수 없습니다.")
         List<OrderDetailRequest> orderDetailRequestList
