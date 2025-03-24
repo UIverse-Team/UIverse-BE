11c9f366f199477b7ec8660d0ebdaebd1308991e
jihaneol <6531z@naver.com>
2025-03-25 00:09:28 +0900

    ✨ feat: jdbcTemplate를 사용한 insert 로그 기능 구현
    - 페이지 방문, 검색 기록, 클릭 로그, 리뷰 조회 로그
    
    🔗 Resolves: #158

--- backend/log/src/main/java/com/example/log/common/util/exampleUtil.java
+++
@@ -1,4 +0,0 @@
-package com.example.log.common.util;
-
-public class exampleUtil {
-}
--- backend/log/src/main/java/com/example/log/repository/LogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.log.repository;
-
-import com.example.log.domain.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogRepository extends JpaRepository<Log, Long> {
-}
