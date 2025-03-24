0dbf6d2ceed4c5cafdeebbfb3cf6bbcb1bf468b7
kiteof_park <kiteof_park@naver.com>
2025-03-24 14:44:48 +0900

    ✨ feat: 🔗 Resolves: #be/feat/157

---
+++ backend/JiShop/src/main/java/com/jishop/config/TossPaymentConfig.java
@@ -0,0 +1,46 @@
+package com.jishop.config;
+
+import lombok.Getter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Getter
+@Configuration
+public class TossPaymentConfig {
+
+    @Value("${payment.toss.client-key}")
+    private String clientKey;
+
+    @Value("${payment.toss.secret-key}")
+    private String secretKey;
+
+    @Value("${payment.toss.confirm-url}")
+    private String confirmUrl;
+
+//    @Value("${payment.toss.success-url}")
+//    private String successUrl;
+//
+//    @Value("${payment.toss.fail-url}")
+//    private String failUrl;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    
+    // 토스 API 호출에 필요한 헤더 생성
+    public HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        String encodedAuth = Base64.getEncoder().encodeToString((secretKey + ":").getBytes(StandardCharsets.UTF_8));
+        headers.set(HttpHeaders.AUTHORIZATION, "Basic " + encodedAuth);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+}
