b5ef1b8900347518b0dafec82df01445b8911574
kiteof_park <kiteof_park@naver.com>
2025-03-25 11:47:55 +0900

    🛠️ refactor: ProductWeight 레포지토리 다시 추가
    🔗 Resolves: #be/feat/161

--- backend/JiShop/src/main/java/com/jishop/productscore/repository/ProductWeightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jishop.productscore.repository;
-
-import com.jishop.productscore.domain.ProductWeight;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductWeightRepository extends JpaRepository<ProductWeight, Long> {
-}
