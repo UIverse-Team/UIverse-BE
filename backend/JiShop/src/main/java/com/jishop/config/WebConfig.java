63afc1d9884cbd2a09b441450e1e87d3b1f080fd
JangBJ <qudwnd1001@naver.com>
2025-03-25 15:19:51 +0900

    🛠️ refactor: WebConfig 수정
    🔗 Resolves: #146

--- backend/JiShop/src/main/java/com/jishop/config/WebConfig.java
+++ backend/JiShop/src/main/java/com/jishop/config/WebConfig.java
@@ -49,7 +49,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     // todo: 추후 반영 결정해야할 사항 (3/23)
-    /*@Bean
+    /* @Bean
     public FilterRegistrationBean<SecurityHeadersFilter> securityHeadersFilter() {
         FilterRegistrationBean<SecurityHeadersFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new SecurityHeadersFilter());
