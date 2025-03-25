45fa5af566348b74e708ffb6f39f45e30690f4f6
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-03-25 10:14:56 +0900

    🛠️ refactor: 코드 리펙토링
    🔗 Resolves: #156

--- backend/JiShop/src/main/java/com/jishop/category/service/CategoryServiceImpl.java
+++ backend/JiShop/src/main/java/com/jishop/category/service/CategoryServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -75,6 +74,6 @@ public class CategoryServiceImpl implements CategoryService {
                             productCount
                     );
                 })
-                .collect(Collectors.toList());
+                .toList();
     }
 }
