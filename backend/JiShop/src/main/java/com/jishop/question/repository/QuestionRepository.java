d3cb7c4a406bff936ba8945e63537f5c11e96bdd
kiteof_park <kiteof_park@naver.com>
2025-03-26 23:07:03 +0900

    🔧 chore: 개행 컨벤션 준수
    🔗 Resolves: #be/feat/1be/feat/181

--- backend/JiShop/src/main/java/com/jishop/question/repository/QuestionRepository.java
+++ backend/JiShop/src/main/java/com/jishop/question/repository/QuestionRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
     List<Question> findAllByUserIdOrderByCreatedAt(Long userId);
 }
