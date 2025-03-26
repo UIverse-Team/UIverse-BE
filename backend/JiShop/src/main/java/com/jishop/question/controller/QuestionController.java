59a9928412b82ce5eb44cc5f4d051fa3ac5de324
kiteof_park <kiteof_park@naver.com>
2025-03-26 23:06:42 +0900

    🔧 chore: 인터페이스 반환타입 수정
    🔗 Resolves: #be/feat/1be/feat/181

--- backend/JiShop/src/main/java/com/jishop/question/controller/QuestionController.java
+++ backend/JiShop/src/main/java/com/jishop/question/controller/QuestionController.java
@@ -6,10 +6,12 @@ import com.jishop.question.dto.QuestionResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 @Tag(name = "문의사항 API")
 public interface QuestionController {
 
     ResponseEntity<?> createQuestion(QuestionRequest request, User user);
-    ResponseEntity<?> getMyQuestion(User user);
+    ResponseEntity<List<QuestionResponse>> getMyQuestion(User user);
     ResponseEntity<QuestionResponse> getQuestion(User user, Long id);
 }
