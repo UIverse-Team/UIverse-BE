8c198fa641ae6f789497335bca9315128a66bdfe
kiteof_park <kiteof_park@naver.com>
2025-03-26 12:40:50 +0900

    ✨ feat: 문의사항, 문의사항 카테고리 레포지토리 구현
    🔗 Resolves: #be/feat/181

---
+++ backend/JiShop/src/main/java/com/jishop/question/repository/QuestionRepository.java
@@ -0,0 +1,13 @@
+package com.jishop.question.repository;
+
+import com.jishop.member.domain.User;
+import com.jishop.question.domain.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findAllByUserIdOrderByCreatedAt(Long userId);
+}
