38eb39de6782b207ba6924b153dc45550d318c75
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-03-28 11:51:34 +0900

    🔧 chore: 스웨거 설명 추가
    🔗 Resolves: #224

--- backend/JiShop/src/main/java/com/jishop/product/controller/ProductController.java
+++ backend/JiShop/src/main/java/com/jishop/product/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.jishop.product.dto.request.ProductRequest;
 import com.jishop.product.dto.response.ProductListResponse;
 import com.jishop.product.dto.response.ProductResponse;
 import com.jishop.product.dto.response.TodaySpecialListResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.web.PagedModel;
 
@@ -13,12 +14,16 @@ import java.util.List;
 @Tag(name = "상품 API")
 public interface ProductController {
 
+    @Operation(summary = "상품 목록 조회")
     PagedModel<ProductListResponse> getProductList(final ProductRequest productRequest, final int page, final int size);
 
     // user가 null일 수 있음(비회원)
+    @Operation(summary = "상품 단건 조회")
     ProductResponse getProduct(final User user, final Long productId);
 
+    @Operation(summary = "인기순(찜순) 상품 조회")
     List<ProductListResponse> getProductByWishTopTen(final int page, final int size);
 
+    @Operation(summary = "오늘의 특가 상품 조회")
     PagedModel<TodaySpecialListResponse> getProductByTodaySpecial(final int page, final int size);
 }
