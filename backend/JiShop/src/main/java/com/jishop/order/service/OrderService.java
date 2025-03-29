18c64d81f0ceb543a41d484be822618c42720cf3
Soyun_p <ehouse190@gmail.com>
2025-03-29 21:49:22 +0900

    ✨ feat: 주문서로 넘어갈 때의 API 추가
    - 장바구니에서 넘어갈 때
    - 바로 주문하기에서 넘어갈 때
    
    🔗 Resolves: #be/feat/243

--- backend/JiShop/src/main/java/com/jishop/order/service/OrderService.java
+++ backend/JiShop/src/main/java/com/jishop/order/service/OrderService.java
@@ -1,8 +1,11 @@
 package com.jishop.order.service;
 
+import com.jishop.cart.dto.CartResponse;
 import com.jishop.member.domain.User;
 import com.jishop.order.dto.*;
 import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface OrderService {
 
@@ -13,6 +16,8 @@ public interface OrderService {
     Page<OrderResponse> getPaginatedOrders(User user, String period, int page, int size);
     void cancelOrder(User user, Long orderId, String orderNumber, String phone);
     OrderCancelResponse getCancelPage(User user, Long orderId, String orderNumber, String phone);
+    CartResponse getCheckout(User user, List<OrderDetailRequest> orderDetailRequest);
+    CartResponse getCheckoutInstant(User user, Long saleProductId, int quantity);
 
     //비회원
     OrderResponse createGuestOrder(OrderRequest orderRequest);
@@ -20,4 +25,6 @@ public interface OrderService {
     OrderDetailPageResponse getGuestOrder(String orderNumber, String phone);
     void cancelGuestOrder(String orderNumber, String phone);
     OrderCancelResponse getGuestCancelPage(String orderNumber, String phone);
+    CartResponse getGuestCheckout(User user, List<OrderDetailRequest> orderDetailRequest);
+    CartResponse getGuestCheckoutInstant(User user,  Long saleProductId, int quantity);
 }
