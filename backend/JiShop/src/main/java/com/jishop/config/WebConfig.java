0624ac4efaba347c1104e62f43ca40b336359a0b
JangBJ <qudwnd1001@naver.com>
2025-03-30 11:53:59 +0900

    🛠️ refactor: 인터셉터 추가
    🔗 Resolves: #172

--- backend/JiShop/src/main/java/com/jishop/config/WebConfig.java
+++ backend/JiShop/src/main/java/com/jishop/config/WebConfig.java
@@ -29,8 +29,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/notices")
-                .excludePathPatterns("/auth/signin", "/signup", "/productwishlist/**", "/storewishlist/**");
+                .addPathPatterns("/notices","/productwishlist/**", "/storewishlist/**")
+                .excludePathPatterns("/auth/signin", "/signup");
     }
 
     @Override
