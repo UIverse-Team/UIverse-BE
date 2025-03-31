13728f7cc86708aaa04bde204050d571aae6f0b7
kiteof_park <kiteof_park@naver.com>
2025-03-31 20:51:35 +0900

    🛠️ refactor: 주석 제거
    🔗 Resolves: #be/feat/157

--- backend/JiShop/src/main/java/com/jishop/payment/domain/EasyPayInfo.java
+++ backend/JiShop/src/main/java/com/jishop/payment/domain/EasyPayInfo.java
@@ -2,6 +2,8 @@ package com.jishop.payment.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class EasyPayInfo {
 
     // TODO: 토스페이 제공자 확인
-        // 토스페이, 카카오페이, 네이퍼페이
+    @Enumerated(EnumType.STRING)
     private PaymentProvider provider;
 
     @Column(name = "easypay_amount")
