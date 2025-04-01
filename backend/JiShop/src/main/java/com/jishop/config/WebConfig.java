9c80cc2388f3609ddd2acb61818daa292ea2405a
kiteof_park <kiteof_park@naver.com>
2025-04-01 13:06:44 +0900

    🐛 fix: 인터셉터에서 /notices 경로 제외
    🔗 Resolves: #be/feat/266

--- backend/JiShop/src/main/java/com/jishop/config/WebConfig.java
+++ backend/JiShop/src/main/java/com/jishop/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/notices","/productwishlist/**", "/storewishlist/**")
+                .addPathPatterns("/productwishlist/**", "/storewishlist/**")
                 .excludePathPatterns("/auth/signin", "/signup");
     }
 
