a206bdd2d51a9052f9914eb1a76c47b7aef8b809
kiteof_park <kiteof_park@naver.com>
2025-04-08 15:59:20 +0900

    🔧 chore: 결제 렌더링 코드 정리
    🔗 Resolves: #be/feat/291

--- backend/JiShop/src/main/java/com/jishop/order/controller/OrderControllerImpl.java
+++ backend/JiShop/src/main/java/com/jishop/order/controller/OrderControllerImpl.java
@@ -22,44 +22,24 @@ public class OrderControllerImpl implements OrderController {
 
     private final OrderService orderService;
 
-    //주문 생성 - 결제 페이지와 연동
+    // 주문 생성
     @Override
     @PostMapping
-    public void createOrder(@CurrentUser User user,
-                            @RequestBody @Valid OrderRequest orderRequest,
-                            HttpServletResponse response) throws IOException {
+    public ResponseEntity<OrderResponse> createOrder(@CurrentUser User user,
+                                                     @Valid @RequestBody OrderRequest orderRequest) {
         OrderResponse orderResponse = orderService.createOrder(user, orderRequest);
-        response.sendRedirect("/orders/checkout/view?orderNumber="
-                + orderResponse.orderNumber()
-                + "&amount=" + orderResponse.totalPrice());
+
+        return ResponseEntity.ok(orderResponse);
     }
-
-//    @Override
-//    @PostMapping
-//    public ResponseEntity<OrderResponse> createOrder(@CurrentUser User user,
-//                                                     @Valid @RequestBody OrderRequest orderRequest) {
-//        OrderResponse orderResponse = orderService.createOrder(user, orderRequest);
-//
-//        return ResponseEntity.ok(orderResponse);
-//    }
-
-    //주문 내역 단건 조회 - 결제 페이지와 연동
+    
+    //주문 내역 단건 조회
     @Override
-    @GetMapping("/{orderNumber}")
-    public ResponseEntity<OrderDetailPageResponse> getOrder(@CurrentUser User user, @PathVariable String orderNumber){
-        OrderDetailPageResponse orderDetailResponse = orderService.getOrder(user, orderNumber, null);
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderDetailPageResponse> getOrder(@CurrentUser User user, @PathVariable Long orderId){
+        OrderDetailPageResponse orderDetailResponse = orderService.getOrder(user, orderId, null, null);
 
         return ResponseEntity.ok(orderDetailResponse);
     }
-    
-//    //주문 내역 단건 조회
-//    @Override
-//    @GetMapping("/{orderId}")
-//    public ResponseEntity<OrderDetailPageResponse> getOrder(@CurrentUser User user, @PathVariable Long orderId){
-//        OrderDetailPageResponse orderDetailResponse = orderService.getOrder(user, orderId, null, null);
-//
-//        return ResponseEntity.ok(orderDetailResponse);
-//    }
 
     //주문 전체 조회 (페이징 처리)
     @Override
@@ -76,13 +56,13 @@ public class OrderControllerImpl implements OrderController {
     }
 
     //주문 취소
-//    @Override
-//    @PatchMapping("/{orderId}")
-//    public ResponseEntity<String> cancelOrder(@CurrentUser User user, @PathVariable Long orderId){
-//        orderService.cancelOrder(user, orderId, null, null);
-//
-//        return ResponseEntity.ok("주문이 취소되었습니다");
-//    }
+    @Override
+    @PatchMapping("/{orderId}")
+    public ResponseEntity<String> cancelOrder(@CurrentUser User user, @PathVariable Long orderId){
+        orderService.cancelOrder(user, orderId, null, null);
+
+        return ResponseEntity.ok("주문이 취소되었습니다");
+    }
 
     // 바로 구매하기
     @Override
@@ -94,13 +74,13 @@ public class OrderControllerImpl implements OrderController {
     }
 
     //회원 주문 취소 상세  페이지
-//    @Override
-//    @GetMapping("/getCancel/{orderId}")
-//    public ResponseEntity<OrderCancelResponse> getOrderCancel(@CurrentUser User user, @PathVariable Long orderId) {
-//        OrderCancelResponse orderCancelResponse = orderService.getCancelPage(user, orderId, null, null);
-//
-//        return ResponseEntity.ok(orderCancelResponse);
-//    }
+    @Override
+    @GetMapping("/getCancel/{orderId}")
+    public ResponseEntity<OrderCancelResponse> getOrderCancel(@CurrentUser User user, @PathVariable Long orderId) {
+        OrderCancelResponse orderCancelResponse = orderService.getCancelPage(user, orderId, null, null);
+
+        return ResponseEntity.ok(orderCancelResponse);
+    }
 
     //장바구니에서 주문서로 넘어갈 때 사용하는 API
     @PostMapping("/checkout")
@@ -120,4 +100,25 @@ public class OrderControllerImpl implements OrderController {
 
         return ResponseEntity.ok(products);
     }
+
+    //주문 생성 - 결제 페이지와 연동
+//    @Override
+//    @PostMapping
+//    public void createOrder(@CurrentUser User user,
+//                            @RequestBody @Valid OrderRequest orderRequest,
+//                            HttpServletResponse response) throws IOException {
+//        OrderResponse orderResponse = orderService.createOrder(user, orderRequest);
+//        response.sendRedirect("/orders/checkout/view?orderNumber="
+//                + orderResponse.orderNumber()
+//                + "&amount=" + orderResponse.totalPrice());
+//    }
+
+    //주문 내역 단건 조회 - 결제 페이지와 연동
+//    @Override
+//    @GetMapping("/{orderNumber}")
+//    public ResponseEntity<OrderDetailPageResponse> getOrder(@CurrentUser User user, @PathVariable String orderNumber){
+//        OrderDetailPageResponse orderDetailResponse = orderService.getOrder(user, orderNumber, null);
+//
+//        return ResponseEntity.ok(orderDetailResponse);
+//    }
 }
