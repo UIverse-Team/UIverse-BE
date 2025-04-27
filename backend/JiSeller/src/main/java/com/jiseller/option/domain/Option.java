2789922c5675f814dd401a691e96169ff8cbc80f
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2025-04-28 05:02:06 +0900

    ✨ feat: (판매자 모듈) 상품 옵션 기능
    🔗 Resolves: #318

---
+++ backend/JiSeller/src/main/java/com/jiseller/option/domain/Option.java
@@ -0,0 +1,26 @@
+package com.jiseller.option.domain;
+
+import com.jiseller.common.util.BaseEntity;
+import com.jishop.option.domain.OptionCategory;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Option extends BaseEntity {
+
+    @Enumerated(EnumType.STRING)
+    private OptionCategory categoryType;
+
+    @Column(name = "option_value", nullable = false)
+    private String optionValue;
+
+    @Column(name = "option_extra", nullable = false, columnDefinition = "integer default 0")
+    private int optionExtra;
+}
